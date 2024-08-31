a5686c5e8a59093c4d0ed5825106c14103a2f303
dannyarnold <dannyarnold2006@hotmail.com>
2024-08-31 15:43:02 -0300

    modificación en sorted.py

--- ordenacion/sorted.py
+++ ordenacion/sorted.py
@@ -1,16 +1,16 @@
 numbers = [3, 6, 1, 8, 2, 9, 5]
-# sorted_numbers = []
-# while numbers:
-#     minimum = numbers[0]  # Set the first element as the current minimum
-#     for x in numbers:
-#         # If any element is smaller than the current minimum, then update the minimum
-#         if x < minimum:
-#             minimum = x
-#         # Add minimum to the sorted_numbers list
-#             sorted_numbers.append(minimum)
-#         # remove minimum from the numbers list
-#     numbers.remove(minimum)
-# print(sorted_numbers)
+sorted_numbers = []
+while numbers:
+    minimum = numbers[0]  # Set the first element as the current minimum
+    for x in numbers:
+        # If any element is smaller than the current minimum, then update the minimum
+        if x < minimum:
+            minimum = x
+        # Add minimum to the sorted_numbers list
+    sorted_numbers.append(minimum)
+        # remove minimum from the numbers list
+    numbers.remove(minimum)
+print(sorted_numbers)
 
 sorted_numbers=sorted(numbers)
 print(sorted_numbers)
