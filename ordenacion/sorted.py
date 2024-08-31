7ed02aa9e69fab741ac907b8ebf0dac156108608
dannyarnold <dannyarnold2006@hotmail.com>
2024-08-31 19:01:51 -0300

    Actualización de sorted.py

--- ordenacion/sorted.py
+++ ordenacion/sorted.py
@@ -1,4 +1,4 @@
-numbers = [3, 6, 1, 8, 2, 9, 5]
+numbers = [3, 6, 1, 8, 2, 9, 99, 101, 5]
 sorted_numbers = []
 while numbers:
     minimum = numbers[0]  # Set the first element as the current minimum
@@ -8,21 +8,26 @@ while numbers:
             minimum = x
         # Add minimum to the sorted_numbers list
     sorted_numbers.append(minimum)
-        # remove minimum from the numbers list
+    # remove minimum from the numbers list
     numbers.remove(minimum)
 print(sorted_numbers)
 
-sorted_numbers=sorted(numbers)
+sorted_numbers = sorted(numbers)
 print(sorted_numbers)
 
 print(numbers)
 numbers.sort()
 print(numbers)
 
-numbersListTuple = [('Ram', '21'), ('Shyam', '10'), ('Sita', '7'), ('Gita', '30')]
+numbersListTuple = [('Ram', '21'), ('Shyam', '10'),
+                    ('Sita', '7'), ('Gita', '30')]
 # print(type(numbersListTuple))
 # print(type(numbersListTuple[0]))
+
+
 def get_second_element_as_int(t):
     return int(t[1])
+
+
 numbersListTuple.sort(key=get_second_element_as_int)
 print(numbersListTuple)
